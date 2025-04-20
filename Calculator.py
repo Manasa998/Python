a09e0b8c1697a9c497e3ed32e8d8f45177558e76
Manasa998 <117272752+Manasa998@users.noreply.github.com>
2025-04-20 23:22:12 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+#this is calculator program using functions
+#functions will follow the main rule 
+# 1) take inputs
+# 2) perform the required action
+# 3) then return the vaule
+def add(num , num1):
+    a = num + num1
+    return a
+
+def sub(num , num1):
+    b = num - num1
+    return b
+
+def mul(num , num1):
+    c = num * num1
+    return c
+
+def div(num , num1):
+    d = num / num1
+    return d
+
+def per(num , num1):
+    e = num % num1
+    return e
+
+print(add(23,34))
+print(sub(3,8))
+print(mul(5,8))
+print(div(5,6))
+print(per(5,7))
